f2778dedd2d0259d4eb74621f47acb8432c2968c
DIP05I <fejer.csa@gmail.com>
2024-01-16 14:40:39 +0100

    2.

--- 2.py
+++ 2.py
@@ -4,3 +4,12 @@ import math
 # modulok gyakorlása
 print(math.pi)
 modulok.udvozles("Aladár")
+
+inputresult = int(input("Írj be egy számot: "))
+# inputresult = input("Írj be egy számot: ")
+
+if not (isinstance(inputresult, int)):
+    raise TypeError("Nem szám típusút adtál meg.")
+else:
+    if inputresult < 10:
+        raise ValueError("10-nél kisebb számot adtál meg.")
--- O1.py
+++ O1.py
@@ -1,7 +1,10 @@
 class Poligon:
-    def __init__(self,sides,color = "red" ):
+    #osztályattribútum, minden példánynak azonos
+    color = "piros"
+
+    def __init__(self,sides,color2 = "red" ):
         self.sides = sides
-        self.color = color
+        self.color2 = color2
 
         # konstruktor első paramétere mindig self
         # ha a paraméternek default értéke van a végére kell kerülnie
@@ -12,6 +15,7 @@ class Triangle:
 
 my_polygon = Poligon(4)
 print(my_polygon.sides)
+print(my_polygon.color2)
 print(my_polygon.color)
 
 
